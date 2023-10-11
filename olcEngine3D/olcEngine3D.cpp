40d810cb2d38fc0a2a7981466cb62548098ecd1e
Corrie <corriejohnstone98@hotmail.com>
2023-10-11 17:15:28 +0100

    Cube rasterizing successfully; added tetrahedron
    Added meshTetrahedron and coords following lower left quadrant CW method as laid out in meshCube.

--- olcEngine3D/olcEngine3D.cpp
+++ olcEngine3D/olcEngine3D.cpp
@@ -1,4 +1,5 @@
 #include "olcConsoleGameEngine.h"
+using namespace std;
 
 struct vec3d {
 	float x, y, z;
@@ -9,11 +10,11 @@ struct triangle {
 };
 
 struct mesh {
-	std::vector<triangle> tris;
+	vector<triangle> tris;
 };
 
 struct matrix4x4 {
-	float m[4][4];
+	float m[4][4] = { 0 };
 };
 
 
@@ -30,9 +31,12 @@ public:
 
 private:
 	mesh meshCube;
+	mesh meshTetrahedron
 	matrix4x4 matProjection;
 
-	void MultiplyMatrixVector(vec3d &in, vec3d &out, matrix4x4 &mat) {
+	float rotAngle;
+
+	void MultiplyMatrices(vec3d &in, vec3d &out, matrix4x4 &mat) {
 		out.x = in.x * mat.m[0][0] + in.y * mat.m[1][0] + in.z * mat.m[2][0] + mat.m[3][0];
 		out.y = in.x * mat.m[0][1] + in.y * mat.m[1][1] + in.z * mat.m[2][1] + mat.m[3][1];
 		out.z = in.x * mat.m[0][2] + in.y * mat.m[1][2] + in.z * mat.m[2][2] + mat.m[3][2];
@@ -49,31 +53,52 @@ public:
 	bool OnUserCreate() override
 	{
 		meshCube.tris = {
+			// SOUTH IS THE CLOSEST FACE PERPENDICULAR TO THE SCREEN
+			// FACES ORIENTED RELATIVE TO THEIR NORMALS
+			// TRIS (triangles ie 3 Vertices) ARE TAKEN CLOCKWISE FROM LOWER LEFT QUADRANT
 
-		// SOUTH
-			{0.0f,0.0f,0.0f,	0.0f,1.0f,0.0f,		1.0f,1.0f,0.0f},
-			{0.0f,0.0f,0.0f,	1.0f,1.0f,0.0f,		1.0f,0.0f,0.0f},
+			// SOUTH FACE
+				{ 0.0f, 0.0f, 0.0f,    0.0f, 1.0f, 0.0f,    1.0f, 1.0f, 0.0f },
+				{ 0.0f, 0.0f, 0.0f,    1.0f, 1.0f, 0.0f,    1.0f, 0.0f, 0.0f },
 
-		// EAST
-			{1.0f,0.0f,0.0f,	1.0f,1.0f,0.0f,		1.0f,1.0f,1.0f},
-			{1.0f,0.0f,0.0f,	1.0f,1.0f,1.0f,		1.0f,0.0f,1.0f},
+			// EAST FACE
+				{ 1.0f, 0.0f, 0.0f,    1.0f, 1.0f, 0.0f,    1.0f, 1.0f, 1.0f },
+				{ 1.0f, 0.0f, 0.0f,    1.0f, 1.0f, 1.0f,    1.0f, 0.0f, 1.0f },
 
-		// NORTH
-			{0.0f,0.0f,1.0f,	0.0f,1.0f,1.0f,		1.0f,1.0f,1.0f},
-			{0.0f,0.0f,1.0f,	1.0f,1.0f,1.0f,		1.0f,0.0f,1.0f},
+			// NORTH FACE
+				{ 1.0f, 0.0f, 1.0f,    1.0f, 1.0f, 1.0f,    0.0f, 1.0f, 1.0f },
+				{ 1.0f, 0.0f, 1.0f,    0.0f, 1.0f, 1.0f,    0.0f, 0.0f, 1.0f },
 
-		// WEST
-			{0.0f,0.0f,1.0f,	0.0f,1.0f,1.0f,		0.0f,1.0f,0.0f},
-			{0.0f,0.0f,1.0f,	0.0f,1.0f,0.0f,		0.0f,0.0f,0.0f},
+			// WEST FACE
+				{ 0.0f, 0.0f, 1.0f,    0.0f, 1.0f, 1.0f,    0.0f, 1.0f, 0.0f },
+				{ 0.0f, 0.0f, 1.0f,    0.0f, 1.0f, 0.0f,    0.0f, 0.0f, 0.0f },
 
-		// TOP
-			{0.0f,1.0f,0.0f,	0.0f,1.0f,1.0f,		1.0f,1.0f,1.0f},
-			{0.0f,1.0f,0.0f,	1.0f,1.0f,1.0f,		1.0f,1.0f,0.0f},
+			// TOP FACE
+				{ 0.0f, 1.0f, 0.0f,    0.0f, 1.0f, 1.0f,    1.0f, 1.0f, 1.0f },
+				{ 0.0f, 1.0f, 0.0f,    1.0f, 1.0f, 1.0f,    1.0f, 1.0f, 0.0f },
 
-		// BOTTOM
-			{0.0f,0.0f,0.0f,	0.0f,0.0f,1.0f,		1.0f,0.0f,1.0f},
-			{0.0f,0.0f,0.0f,	1.0f,0.0f,1.0f,		1.0f,0.0f,0.0f},
+			// BOTTOM FACE
+				{ 1.0f, 0.0f, 1.0f,    0.0f, 0.0f, 1.0f,    0.0f, 0.0f, 0.0f },
+				{ 1.0f, 0.0f, 1.0f,    0.0f, 0.0f, 0.0f,    1.0f, 0.0f, 0.0f },
+
 		};
+
+		meshTetrahedron.tris = {
+			// VERTS
+			// 1(1.0f, -1.0f, -1.0f)	2(-1.0f,-1.0f,1.0f)		3(1.0f,1.0f,1.0f)	4(-1.0f, 1.0f, -1.0f)
+
+			// FACE A 1-2-3
+				{1.0f, -1.0f, -1.0f,	-1.0f,-1.0f,1.0f,		1.0f,1.0f,1.0f},
+
+			// FACE B 1-4-2
+				{1.0f, -1.0f, -1.0f,   -1.0f, 1.0f, -1.0f,		-1.0f,-1.0f,1.0f},
+
+			// FACE C 4-3-2
+				{-1.0f, 1.0f, -1.0f,    1.0f,1.0f,1.0f,    -1.0f,-1.0f,1.0f},
+
+			// FACE D 1-4-3
+				{1.0f, -1.0f, -1.0f,    -1.0f, 1.0f, -1.0f,    1.0f,1.0f,1.0f},
+		}
 
 		// Projection Matrix
 		float zNear = 0.1f;
@@ -94,21 +119,58 @@ public:
 
 	bool OnUserUpdate(float elapsedTime) override
 	{
-
 		Fill(0, 0, ScreenWidth(), ScreenHeight(), PIXEL_SOLID, FG_BLACK);
 
-		//Draw Triangles
-		for (auto tri : meshCube.tris) {
-			triangle triProjected;
-			MultiplyMatrixVector(tri.p[0], triProjected.p[0], matProjection);
-			MultiplyMatrixVector(tri.p[1], triProjected.p[1], matProjection);
-			MultiplyMatrixVector(tri.p[2], triProjected.p[2], matProjection);
+		matrix4x4 matRotZ, matRotX;
+		rotAngle += 1.0f * elapsedTime;
+
+		// Z-Axis Rotation Matrix 
+		matRotZ.m[0][0] = cosf(rotAngle);
+		matRotZ.m[0][1] = sinf(rotAngle);
+		matRotZ.m[1][0] = -sinf(rotAngle);
+		matRotZ.m[1][1] = cosf(rotAngle);
+		matRotZ.m[2][2] = 1;
+		matRotZ.m[3][3] = 1;
+
+		// X-Axis Rotation Matrix
+		matRotX.m[0][0] = 1;
+		matRotX.m[1][1] = cosf(rotAngle * 0.5f);
+		matRotX.m[1][2] = sinf(rotAngle * 0.5f);
+		matRotX.m[2][1] = -sinf(rotAngle * 0.5f);
+		matRotX.m[2][2] = cosf(rotAngle * 0.5f);
+		matRotX.m[3][3] = 1;
+
+
+		// Draw Triangles
+		for (auto tri : meshCube.tris)
+		{
+			triangle triProjected, triTranslated, triRotatedZ, triRotatedZX;
+
+			// Rotate on Z-Axis
+			MultiplyMatrices(tri.p[0], triRotatedZ.p[0], matRotZ);
+			MultiplyMatrices(tri.p[1], triRotatedZ.p[1], matRotZ);
+			MultiplyMatrices(tri.p[2], triRotatedZ.p[2], matRotZ);
+
+			// Rotate on X-Axis
+			MultiplyMatrices(triRotatedZ.p[0], triRotatedZX.p[0], matRotX);
+			MultiplyMatrices(triRotatedZ.p[1], triRotatedZX.p[1], matRotX);
+			MultiplyMatrices(triRotatedZ.p[2], triRotatedZX.p[2], matRotX);
+
+			// Offset on Z-Axis as to be inside view
+			triTranslated = triRotatedZX;
+			triTranslated.p[0].z = triRotatedZX.p[0].z + 3.0f;
+			triTranslated.p[1].z = triRotatedZX.p[1].z + 3.0f;
+			triTranslated.p[2].z = triRotatedZX.p[2].z + 3.0f;
+
+			// Project triangles from 3D onto 2D plane (ie screen)
+			MultiplyMatrices(triTranslated.p[0], triProjected.p[0], matProjection);
+			MultiplyMatrices(triTranslated.p[1], triProjected.p[1], matProjection);
+			MultiplyMatrices(triTranslated.p[2], triProjected.p[2], matProjection);
 
 			// Scale into view
 			triProjected.p[0].x += 1.0f; triProjected.p[0].y += 1.0f;
 			triProjected.p[1].x += 1.0f; triProjected.p[1].y += 1.0f;
 			triProjected.p[2].x += 1.0f; triProjected.p[2].y += 1.0f;
-
 			triProjected.p[0].x *= 0.5f * (float)ScreenWidth();
 			triProjected.p[0].y *= 0.5f * (float)ScreenHeight();
 			triProjected.p[1].x *= 0.5f * (float)ScreenWidth();
@@ -116,22 +178,26 @@ public:
 			triProjected.p[2].x *= 0.5f * (float)ScreenWidth();
 			triProjected.p[2].y *= 0.5f * (float)ScreenHeight();
 
-			DrawTriangle(
-				triProjected.p[0].x, triProjected.p[0].y,
+			// Rasterize triangle
+			DrawTriangle(triProjected.p[0].x, triProjected.p[0].y,
 				triProjected.p[1].x, triProjected.p[1].y,
 				triProjected.p[2].x, triProjected.p[2].y,
 				PIXEL_SOLID, FG_WHITE);
+
 		}
 
 		return true;
 	}
+
 };
 
 
-int main() {
 
+
+int main()
+{
 	olcEngine3D demo;
 	if (demo.ConstructConsole(256, 240, 4, 4))
 		demo.Start();
-	return 0;
+    return 0;
 }
